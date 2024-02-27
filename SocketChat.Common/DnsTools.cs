2273017aaea702c9750e19482470e9207d4e1079
Simon <simonchristensen03@gmail.com>
2024-02-27 08:54:15 +0100

    Added common constant and dnstools

---
+++ SocketChat.Common/DnsTools.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace SocketChat.Common;
+
+public static class DnsTools
+{
+    public static async Task<IPAddress> GetLocalIpAddressAsync()
+    {
+        var ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
+        var ipAddress = ipHostInfo.AddressList[0];
+        return ipAddress;
+    }
+}
---
+++ SocketChat.Common/SocketConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace SocketChat.Common;
+
+public static class SocketConstants
+{
+    public const string EndOfMessage = "<|EOM|>";
+    public const string Acknowledgment = "<|ACK|>";
+    public const string Subscribe = "<|SUB|>";
+    public const string Unsubscribe = "<|UNSUB|>";
+    public const string Message = "<|MSG|>";
+    public const int BufferSize = 1_024;
+}
