fa1ef254c1def06d86fd5b535e71ed996d24e864
Simon <simonchristensen03@gmail.com>
2024-02-27 08:54:26 +0100

    Added SocketServer and SocketConnection

---
+++ SocketChat.Server/SocketConnection.cs
@@ -0,0 +1,90 @@
+﻿using SocketChat.Common;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketChat.Server;
+
+public class SocketConnection(Socket socket) 
+    : IDisposable
+{
+    public string Id { get; } = socket.RemoteEndPoint?.ToString() ?? Guid.NewGuid().ToString();
+
+    private readonly Socket _socket = socket;
+    private bool _disposed = false;
+
+    public async Task<bool> SendAsync(string message, CancellationToken cancellationToken = default)
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
+        await SendWithoutAcknowledgmentAsync(message, cancellationToken);
+        var response = await ReceiveWithoutAcknowledgmentAsync(cancellationToken);
+        return response == SocketConstants.Acknowledgment;
+    }
+
+    public async Task<string?> ReceiveAsync(CancellationToken cancellationToken = default)
+    {
+        ObjectDisposedException.ThrowIf(_disposed, this);
+
+        string? response = await ReceiveWithoutAcknowledgmentAsync(cancellationToken);
+
+        if (response == null)
+            return null;
+
+        await SendWithoutAcknowledgmentAsync(SocketConstants.Acknowledgment, cancellationToken);
+        return response;
+    }
+
+    private async Task SendWithoutAcknowledgmentAsync(string message, CancellationToken cancellationToken = default)
+    {
+        var messageBytes = Encoding.UTF8.GetBytes(message + SocketConstants.EndOfMessage);
+        await _socket.SendAsync(messageBytes, SocketFlags.None, cancellationToken);
+    }
+
+    private async Task<string?> ReceiveWithoutAcknowledgmentAsync(CancellationToken cancellationToken = default)
+    {
+        var sb = new StringBuilder();
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var buffer = new byte[SocketConstants.BufferSize];
+            var received = await _socket.ReceiveAsync(buffer, SocketFlags.None, cancellationToken);
+            if (received == 0)
+                break;
+
+            string message = Encoding.UTF8.GetString(buffer, 0, received);
+
+            if (message.IndexOf(SocketConstants.EndOfMessage) > -1)
+            {
+                message = message.Replace(SocketConstants.EndOfMessage, "");
+                sb.Append(message);
+                return sb.ToString();
+            }
+            else
+            {
+                sb.Append(message);
+            }
+        }
+
+        return null;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (_disposed)
+            return;
+
+        if (disposing)
+        {
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+            _socket.Dispose();
+        }
+
+        _disposed = true;
+    }
+}
---
+++ SocketChat.Server/SocketServer.cs
@@ -0,0 +1,91 @@
+﻿using SocketChat.Common;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketChat.Server;
+
+internal class SocketServer
+{
+    private CancellationTokenSource? _cts;
+
+    private readonly Socket _socket;
+
+    private readonly ConcurrentDictionary<string, List<string>> _subscripedConnections = new();
+    private readonly ConcurrentDictionary<string, SocketConnection> _connections = new();
+
+    public SocketServer(IPEndPoint endPoint)
+    {
+        _socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        _socket.Bind(endPoint);
+    }
+
+    public SocketServer(IPAddress address, int port)
+        : this(new IPEndPoint(address, port))
+    {
+    }
+
+    public static async Task<SocketServer> CreateAsync(int port)
+    {
+        var ipAddress = await DnsTools.GetLocalIpAddressAsync();
+        return new(ipAddress, port);
+    }
+
+    public void Start()
+    {
+        _socket.Listen(100);
+
+        _cts = new CancellationTokenSource();
+        var cancellationToken = _cts.Token;
+        Task.Run(() => Run(cancellationToken), cancellationToken);
+    }
+
+    public void Stop()
+    {
+        _cts?.Cancel();
+        _socket.Close();
+    }
+
+    private async Task Run(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            Socket handler = await _socket.AcceptAsync(cancellationToken);
+            SocketConnection connection = new(handler);
+            _connections.TryAdd(connection.Id, connection);
+            Task.Run(() => HandleConnection(connection, cancellationToken), cancellationToken);
+        }
+    }
+
+    private async Task HandleConnection(SocketConnection connection, CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            string? message = await connection.ReceiveAsync(cancellationToken);
+            if (message == null)
+                break;
+
+            if (message.StartsWith(SocketConstants.Subscribe))
+            {
+                var roomId = message.Replace(SocketConstants.Subscribe, "").Trim();
+                _subscripedConnections.AddOrUpdate(connection.Id, [roomId], (connectionId, roomIds) =>
+                {
+                    roomIds.Add(roomId);
+                    return roomIds;
+                });
+            }
+            else if (message.StartsWith(SocketConstants.Unsubscribe))
+            {
+                var roomId = message.Replace(SocketConstants.Unsubscribe, "").Trim();
+                if (_subscripedConnections.TryGetValue(connection.Id, out List<string>? roomIds))
+                {
+                    roomIds.Remove(roomId);
+                }
+            }
+            else if (message.StartsWith(SocketConstants.Message))
+            {
+                // TODO: Send message to all subscribers
+            }
+        }
+    }
+}
